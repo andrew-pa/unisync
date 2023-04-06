d8736f79aab0f619dee46a11022ea32fa7083be7
Mark Roth <mark.roth@walmart.com>
2023-04-06 12:28:35 -0600

    adding main activity structure

---
+++ app/app/src/main/java/com/lightspeed/app/ui/LoginFragment.kt
@@ -0,0 +1,12 @@
+package com.lightspeed.app.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.lightspeed.app.databinding.LoginFragmentBinding
+
+class LoginFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
+        LoginFragmentBinding.inflate(inflater, container, false).root
+}

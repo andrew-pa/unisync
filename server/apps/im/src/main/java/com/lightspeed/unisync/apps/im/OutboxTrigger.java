24bb698dc8d88ff53ce536894e3aae0dbad7bf98
Andrew Palmer <andrew.pa@outlook.com>
2023-04-10 12:13:33 -0600

    start thinking about outbox trigger

--- server/apps/im/src/main/java/com/lightspeed/unisync/apps/im/OutboxTrigger.java
+++ server/apps/im/src/main/java/com/lightspeed/unisync/apps/im/OutboxTrigger.java
@@ -1,9 +1,16 @@
 package com.lightspeed.unisync.apps.im;
 
+import com.lightspeed.unisync.core.interfaces.DataAccess;
 import com.lightspeed.unisync.core.interfaces.Trigger;
 import com.lightspeed.unisync.core.model.Row;
 
 public class OutboxTrigger implements Trigger {
+    final DataAccess dataAccess;
+
+    public OutboxTrigger(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
     public void onRowCreated(Row r) {
     }
 
--- server/apps/im/src/main/java/com/lightspeed/unisync/apps/im/SyncLambda.java
+++ server/apps/im/src/main/java/com/lightspeed/unisync/apps/im/SyncLambda.java
@@ -3,6 +3,7 @@ package com.lightspeed.unisync.apps.im;
 import com.lightspeed.unisync.aws.AbstractSyncLambda;
 import com.lightspeed.unisync.aws.DynamoDataAccess;
 import com.lightspeed.unisync.core.SyncService;
+import com.lightspeed.unisync.core.interfaces.DataAccess;
 import software.amazon.awssdk.regions.Region;
 
 import java.util.Map;
@@ -11,8 +12,8 @@ import java.util.Optional;
 public class SyncLambda extends AbstractSyncLambda {
     @Override
     protected SyncService createService() {
-        return new SyncService(
-                new DynamoDataAccess(Region.US_EAST_1),
+        DataAccess data = new DynamoDataAccess(Region.US_EAST_1);
+        return new SyncService(data,
                 Optional::of, // pass through session IDs as user IDs
                 Map.of());
     }

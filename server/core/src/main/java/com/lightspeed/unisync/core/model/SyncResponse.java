471820f411b29acf20f18da1103f4d412aeb6dfe
Andrew Palmer <andrew.pa@outlook.com>
2023-04-13 10:48:31 -0600

    add getters & setters to sync req/resp

--- server/core/src/main/java/com/lightspeed/unisync/core/model/SyncResponse.java
+++ server/core/src/main/java/com/lightspeed/unisync/core/model/SyncResponse.java
@@ -3,9 +3,33 @@ package com.lightspeed.unisync.core.model;
 import java.util.Set;
 
 public class SyncResponse {
-    public final Set<Integer> deletedRows;
-    public final Set<Row> newOrModifiedRows;
-    public final Set<InvalidRow> invalidRows;
+    public Set<Integer> getDeletedRows() {
+        return deletedRows;
+    }
+
+    public void setDeletedRows(Set<Integer> deletedRows) {
+        this.deletedRows = deletedRows;
+    }
+
+    public Set<Row> getNewOrModifiedRows() {
+        return newOrModifiedRows;
+    }
+
+    public void setNewOrModifiedRows(Set<Row> newOrModifiedRows) {
+        this.newOrModifiedRows = newOrModifiedRows;
+    }
+
+    public Set<InvalidRow> getInvalidRows() {
+        return invalidRows;
+    }
+
+    public void setInvalidRows(Set<InvalidRow> invalidRows) {
+        this.invalidRows = invalidRows;
+    }
+
+    public Set<Integer> deletedRows;
+    public Set<Row> newOrModifiedRows;
+    public Set<InvalidRow> invalidRows;
 
     public SyncResponse(Set<Integer> deletedRows, Set<Row> newOrModifiedRows, Set<InvalidRow> invalidRows) {
         this.deletedRows = deletedRows;
